f43e56ef3d65902553eaba3add2671064e1a2e75
Leandro Arturi <lea.arturi@gmail.com>
2022-02-21 21:31:11 -0300

    Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
         <div className="contenedor">
+           <div className={styles.barra}>
             <div>
+                <Link href="/" passHref>
+                  <Image src="/img/logo.svg" width={400} height={100} alt="Imagen Logo" />
+                </Link>
+              </div>
 
-            </div>
-
-            <nav className={styles.navegacion}>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Nosotros</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/tienda">Tienda</Link>
-            </nav>
+              <nav className={styles.navegacion}>
+                  <Link href="/">Inicio</Link>
+                  <Link href="/nosotros">Nosotros</Link>
+                  <Link href="/blog">Blog</Link>
+                  <Link href="/tienda">Tienda</Link>
+              </nav>
+           </div>
         </div>
     </header>
   )
