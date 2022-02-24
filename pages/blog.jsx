7294160d37c22db85a8e4d0808fbdc27931c3784
Leandro Arturi <lea.arturi@gmail.com>
2022-02-23 21:45:55 -0300

    Get Blog Entries from Strapi

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,11 +1,27 @@
 import Layout from '../components/Layout';
 
-const Blog = () => {
+const Blog = ({entradas}) => {
+
+   console.log(entradas);
+
    return (
       <Layout pagina='Blog'>
          <h1 className='heading'>Blog</h1>
       </Layout>
    );
 };
+
+export async function getServerSideProps() {
+
+   const url = 'http://localhost:1337/blogs';
+   const response = await fetch(url);
+   const entradas = await response.json();
+
+   return {
+      props: {
+         entradas
+      }
+   }
+}
 
 export default Blog;
