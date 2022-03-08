3799d48340f6ef04677bf178338eadfd0f0fef54
Leandro Arturi <lea.arturi@gmail.com>
2022-03-07 21:12:10 -0300

    Actualizar cantidades producto carrito

--- pages/_app.js
+++ pages/_app.js
@@ -8,39 +8,48 @@ function MyApp({ Component, pageProps }) {
     const [carrito, setCarrito] = useState([]);
 
     useEffect(() => {
-      const carritoLS = JSON.parse(localStorage.getItem('carrito')) ?? [];
-      console.log(carritoLS);
-      setCarrito(carritoLS);
+        const carritoLS = JSON.parse(localStorage.getItem('carrito')) || [];
+        console.log(carritoLS);
+        setCarrito(carritoLS);
     }, []);
 
     useEffect(() => {
-      localStorage.setItem('carrito', JSON.stringify(carrito));
+        localStorage.setItem('carrito', JSON.stringify(carrito));
     }, [carrito]);
 
     const addCarrito = producto => {
-
-      if (carrito.some(articulo => articulo.id === producto.id)) {
-        const carritoActualizado = carrito.map(articulo => {
-          if (articulo.id === producto.id) {
-            articulo.cantidad = producto.cantidad + articulo.cantidad;
-          }
-          return articulo;
-        }); 
-        setCarrito(carritoActualizado);
-      } else {
-        setCarrito([...carrito, producto]);
-      }
-
+        if (carrito.some(articulo => articulo.id === producto.id)) {
+            const carritoActualizado = carrito.map(articulo => {
+                if (articulo.id === producto.id) {
+                    articulo.cantidad = producto.cantidad;
+                }
+                return articulo;
+            });
+            setCarrito(carritoActualizado);
+        } else {
+            setCarrito([...carrito, producto]);
+        }
     };
 
-    return (
-      <Component 
-        {...pageProps }
+    const actualizarCantidad = producto => {
+        const carritoActualizado = carrito.map(articulo => {
+            if (articulo.id === producto.id) {
+                articulo.cantidad = producto.cantidad;
+            }
+            return articulo;
+        });
+
+        setCarrito(carritoActualizado);
+    }
+
+    return ( <
+        Component {...pageProps }
         carrito = { carrito }
         addCarrito = { addCarrito }
-      />
+        actualizarCantidad = { actualizarCantidad }
+        />
     );
-    
+
 }
 
 export default MyApp;
--- pages/carrito.jsx
+++ pages/carrito.jsx
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import Image from "next/image";
 import styles from '../styles/Carrito.module.css';
 
-const Carrito = ({carrito}) => {
+const Carrito = ({ carrito, actualizarCantidad }) => {
 
   console.log(carrito);
 
@@ -31,7 +31,25 @@ const Carrito = ({carrito}) => {
 
                         <div>
                           <p className={styles.nombre}>{producto.nombre}</p>
-                          <p className={styles.cantidad}>{producto.cantidad}</p>
+                         
+                          <div className={styles.cantidad}>
+                            <p>Cantidad:</p>
+                            <select 
+                              value={producto.cantidad}
+                              className={styles.select}
+                              onChange={e => actualizarCantidad({
+                                id: producto.id,
+                                cantidad: parseInt(e.target.value)
+                              })}
+                            >
+                              <option value='1'>1</option>
+                              <option value='2'>2</option>
+                              <option value='3'>3</option>
+                              <option value='4'>4</option>
+                              <option value='5'>5</option>
+                            </select>
+                          </div>
+
                           <p className={styles.precio}><span>${producto.precio}</span></p>
                           <p className={styles.subtotal}>Subtotal: $ <span>{producto.precio * producto.cantidad}</span></p>
                         </div>
